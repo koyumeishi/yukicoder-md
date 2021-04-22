d9e54a85eb20735dd5c678c7c39425f68a207c5b
koyumeishi <koyumeishi@users.noreply.github.com>
2021-04-22 12:32:26 +0000

    deploy: ce2080660ef4f111914daaa94ef403159d7cfc87

---
+++ _next/static/tS_kZ-28EcZrDWRKbdwyV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-4b36130ab6c33ce7529d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-70f78207d426c5c57dd4.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
